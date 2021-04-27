19d88f44fe02d27827e0c556de8b85e5715db60e
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-04-27 16:51:39 -0300

    Changed mail logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="../../../../img/logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
