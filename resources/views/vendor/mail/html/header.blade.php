bd8355c571bfcfe3f6431564a2c1746bb2fcdb46
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-04-27 17:29:23 -0300

    Reestructuring of the mail structure

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,10 +1,10 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-            @else
+            @if (trim($slot) === 'Family Size Restaurant')
                 <img src="{{asset('img/logo.png')}}" class="logo" alt="Family Size Logo">
+            @else
+                {{$slot}}
             @endif
         </a>
     </td>
