277b9828f5f00ffd79422942ee14e2d552ec6452
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-04-30 17:42:03 -0300

    Defined database migrations for every table

---
+++ database/migrations/2021_04_30_194049_create_personnel_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonnelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnel', function (Blueprint $table) {
+            $table->id();
+            $table->integer('role_id');
+            $table->foreing('role_id')->references('id')->on('roles');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username');
+            $table->foreign('username')->references('name')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnel');
+    }
+}
---
+++ database/migrations/2021_04_30_194300_create_item_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_order', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->integer('table_id');
+            $table->foreign('table_id')->references('id')->on('tables');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_order');
+    }
+}
