885d5cf7268a0959480f6296920621597bd2d3b8
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-05-12 20:40:01 -0300

    DBSeeder fixed order, TEST 6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [
                 UserSeeder::class,
-                ItemOrderSeeder::class,
+                ItemSeeder::class,
                 TableSeeder::class,
                 OrderSeeder::class,
                 PersonnelSeeder::class,
