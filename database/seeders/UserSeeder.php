24b3ba3988ed5be27d19703fbf2453370fa3b5d4
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-05-12 20:34:20 -0300

    Added email_verified_at for every user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,33 +15,33 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create(array(
-            'email' => 'test@test.com',
-            'name' => 'test',
-            'password' => bcrypt('test')
-        ));
-        User::create(array(
             'email' => 'pepe@test.com',
             'name' => 'pepe',
+            'email_verified_at' => now(),
             'password' => bcrypt('test')
         ));
         User::create(array(
             'email' => 'luis@test.com',
             'name' => 'luis',
+            'email_verified_at' => now(),
             'password' => bcrypt('test')
         ));
         User::create(array(
             'email' => 'carlos@test.com',
             'name' => 'carlos',
+            'email_verified_at' => now(),
             'password' => bcrypt('test')
         ));
         User::create(array(
             'email' => 'daina@test.com',
             'name' => 'daina',
+            'email_verified_at' => now(),
             'password' => bcrypt('test')
         ));
         User::create(array(
             'email' => 'lucia@test.com',
             'name' => 'lucia',
+            'email_verified_at' => now(),
             'password' => bcrypt('test')
         ));
     }
