2b3232556a4544cbb8669b931b4272160377ce6d
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-05-17 11:58:51 -0300

    Table was not repsonsive, resorting to cards

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -5,110 +5,63 @@
         </h2>
     </x-slot>
 
-    <div class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Name - Cuisine
-                    </th>
+    <div class="flex items-center justify-center">
+        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
+            @foreach ($items as $item)
+            <div class="relative bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+                <div class="mt-8">
+                    <p class="text-xl font-semibold my-2">{{$item->name}}</p>
 
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Cost
-                    </th>
+                    <div class="flex space-x-2 text-gray-400 text-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                         <p>{{$item->cuisine}}</p> 
+                    </div>
 
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Vegetarian
-                    </th>
+                    <div class="flex space-x-2 text-gray-900 text-sm my-3">
+                         <p>Vegetarian: {{$item->is_vege ? 'Yes' : 'No'}}</p> 
+                    </div>
 
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Vegan
-                    </th>
+                    <div class="flex space-x-2 text-gray-900 text-sm my-3">
+                        <p>Vegan: {{$item->is_vegan ? 'Yes' : 'No'}}</p> 
+                    </div>
 
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Coeliac
-                    </th>
+                    <div class="flex space-x-2 text-gray-900 text-sm my-3">
+                        <p>Coeliac: {{$item->is_coeliac ? 'Yes' : 'No'}}</p> 
+                    </div>
 
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Alcohol
-                    </th>
+                    <div class="flex space-x-2 text-gray-900 text-sm my-3">
+                        <p>Alcohol: {{$item->has_alcohol ? 'Yes' : 'No'}}</p> 
+                    </div>  
 
-                    <th scope="col" class="relative px-6 py-3">
-                        <span class="sr-only">Edit</span>
-                    </th>
+                    <div class="border-t-2"></div>
+    
+                    <div class="flex justify-between">
+                        <div class="my-2">
+                            <p class="font-semibold text-base mb-2">Cost</p>
 
-                    <th scope="col" class="relative px-6 py-3">
-                        <span class="sr-only">Delete</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
-                    @foreach ($items as $item)
-                        <tr>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <div class="flex items-center">
-                                    <div class="ml-4">
-                                        <div class="text-sm font-medium text-gray-900">
-                                            {{$item->name}}
-                                        </div>
+                            <div class="text-base text-gray-400 font-semibold">
+                                <p>{{$item->cost}}</p>
+                            </div>
+                        </div>
 
-                                        <div class="text-sm text-gray-500">
-                                            {{$item->cuisine}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <div class="text-sm text-gray-900">
-                                    {{$item->cost}}
-                                </div>
-                            </td>
-
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    {{$item->is_vege ? 'Yes' : 'No'}}
-                                </span>
-                            </td>
-
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    {{$item->is_vegan ? 'Yes' : 'No'}}
-                                </span>
-                            </td>
-
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    {{$item->is_coeliac ? 'Yes' : 'No'}}
-                                </span>
-                            </td>
-
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    {{$item->has_alcohol ? 'Yes' : 'No'}}
-                                </span>
-                            </td>
-
-                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <div class="my-2">
+                            <div class="text-base font-semibold whitespace-nowrap text-sm font-medium">
                                 <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            </div>
+                        </div>
+
+                        <div class="my-2">
+                            <div class="text-base font-semibold whitespace-nowrap text-sm font-medium">
                                 <a href="#" class="text-red-600 hover:text-red-900">Delete</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    <tr>
-                        <x-button>
-                            Add Item
-                        </x-button>
-                    </tr>
-                </tbody>
-              </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            @endforeach
         </div>
-      </div>
+    </div>
 </x-app-layout>
