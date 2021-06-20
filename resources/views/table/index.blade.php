f600e5ccfa30dc0d0a722bc5a471ef8b349f470c
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-06-19 21:42:00 -0300

    Added every view for the tables

--- resources/views/table/index.blade.php
+++ resources/views/table/index.blade.php
@@ -1,38 +1,67 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="w-full flex justify-between">
+        <div class="w-full flex justify-between flex-col xs:flex-row text-center xs:text-left">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Tables') }}
             </h2>
-            <x-button>
+
+            <x-link-button class="justify-center xs:justify-start" href="{{url('/tables/create')}}">
                 Add Table
-            </x-button>
+            </x-link-button>
         </div>
     </x-slot>
 
-    <table>
-        <tr>
-            <th>Number</th>
-            <th>People</th>
-            <th>Description</th>
-        </tr>
-        <tbody>
-            @foreach ($tables as $table)
+    <div class="flex flex-col w-full pt-2 px-2 mx-auto overflow-auto">
+        <table class="table-auto text-center divide-y divide-gray-200">
+            <thead class="bg-gray-50">
                 <tr>
-                    <td>
-                        <a href="/tables/{{$table->id}}">{{$table->id}}</a>
-                    </td>
+                    <th scope="col" class="px-4 py-3 text-s font-medium tracking-widest text-gray-500 uppercase">
+                        Number
+                    </th>
 
-                    <td>
-                        {{$table->people}}
-                    </td>
+                    <th scope="col" class="px-4 py-3 text-xs font-medium tracking-widest text-gray-500 uppercase">
+                        People
+                    </th>
 
-                    <td>
-                        {{$table->description}}
-                    </td>
-
+                    <th scope="col" class="px-4 py-3 text-xs font-medium tracking-widest text-gray-500 uppercase">
+                        Description
+                    </th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+
+            <tbody class="bg-white divide-y divide-gray-200">
+                @foreach ($tables as $table)
+                <tr>
+                    <td class="px-4 py-4 whitespace-nowrap">
+                        <div class="flex items-center text-sm font-medium text-gray-900 justify-center">
+                            <a class="hover:border-gray-300 underline" href="/tables/{{$table->id}}">{{$table->id}}</a>
+                        </div>
+                    </td>
+
+                    <td class="px-4 py-4 whitespace-nowrap">
+                        <div class="text-sm text-gray-900">{{$table->people}}</div>
+                    </td>
+
+                    <td class="px-4 py-4 whitespace-nowrap">
+                        <span class="px-4 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                            {{$table->description}}
+                        </span>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <x-success-toast>
+            {{$message}}
+        </x-success-toast>
+    @endif
+
+    @if ($message = Session::get('error'))
+        <x-error-toast>
+            {{$message}}
+        </x-error-toast>
+    @endif
 </x-app-layout>
