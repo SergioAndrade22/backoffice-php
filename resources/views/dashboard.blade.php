1ebd88fabfbafd8a37078dd662e7130280562ba0
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-06-26 19:50:26 -0300

    Added personalized and notifiying welcome message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    @if (count(Auth::user()->getRoleNames()) != 0)
+                        Welcome back <span class="capitalize">{{ Auth::user()->name }}!</span>
+                    @else
+                        You still have no role assigned, contact your manager or sysadmin!
+                    @endif
                 </div>
             </div>
         </div>
